39767e21b5fdcf79128f12cbad34f2364443b5ba
Arnon Sela <arnon@acrisel.com>
2017-10-05 23:33:24 -0500

    sort logging across hosts

--- acrilog/acrilog/nwlogger_socket_handler.py
+++ acrilog/acrilog/nwlogger_socket_handler.py
@@ -96,7 +96,7 @@ class NwLoggerClientHandler(logging.Handler):
         #print('running SSHPipe:', ssh_host, command)
         
         sshname = '{}.sshpipe.log'.format(logger_info['name'])
-        self.sshpipe = sshutil.SSHPipe(ssh_host, command, name=sshname, get_logger=logger_process_lambda(NwLogger, self.logger_info))
+        self.sshpipe = sshutil.SSHPipe(ssh_host, command, name=sshname,) # get_logger=logger_process_lambda(NwLogger, self.logger_info))
         self.logger = logger
         if logger:
             logger.debug("Starting remote logger SSHPipe on host: {}, command: {}".format(ssh_host, command))
