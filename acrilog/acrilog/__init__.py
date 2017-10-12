5ff314b99ac6033295ab80036015294e70968455
Arnon Sela <arnon@acrisel.com>
2017-10-12 09:00:30 -0500

    remote log example

--- acrilog/acrilog/__init__.py
+++ acrilog/acrilog/__init__.py
@@ -1,8 +1,8 @@
 __author__='arnon'
 
 from acrilog.mplogger import MpLogger, create_stream_handler
-from acrilog.timed_sized_logging_handler import TimedSizedRotatingHandler
+from acrilib import TimedSizedRotatingHandler
 from acrilog.nwlogger_socket_server import NwLogger
 from acrilog.nwlogger_socket_handler import NwLoggerClientHandler
-from acrilog.utils import get_free_port, get_ip_address, get_hostname, hostname_resolves
-from acrilog.formatters import LoggerAddHostFilter, LevelBasedFormatter, MicrosecondsDatetimeFormatter
+from acrilib import get_free_port, get_ip_address, get_hostname, hostname_resolves
+from acrilib import LoggerAddHostFilter, LevelBasedFormatter, MicrosecondsDatetimeFormatter
