7fab11a6319de9a1b03cc65ae0e34b625c1560d5
Arnon Sela <arnon@acrisel.com>
2017-11-22 17:51:22 -0600

    queue thread listener

--- acrilog/acrilog_examples/sshlogger_remote.py
+++ acrilog/acrilog_examples/sshlogger_remote.py
@@ -26,7 +26,8 @@ def main(port):
 
         }
 
-    handler = SSHLoggerClientHandler(logger_info=logger_info, ssh_host='arnon-mbp-acris', verbose=True) #, logger=module_logger)
+    handler = SSHLoggerClientHandler(
+        logger_info=logger_info, ssh_host='arnon-mbp-acris', verbose=True)
     logger = logging.getLogger('example.e1')
     logger.addHandler(handler)
     logger.setLevel(logging.DEBUG)
@@ -39,8 +40,9 @@ def main(port):
     logger.warning('Jail zesty vixen who grabbed pay from quack.')
     logger.debug('Quick zephyrs blow, vexing daft Jim.')
     logger.error('The five boxing wizards jump quickly.')
-    
+
     print('done logging.')
+
 
 def cmdargs():
     import argparse
@@ -48,7 +50,8 @@ def cmdargs():
     filename = os.path.basename(__file__)
     progname = filename.rpartition('.')[0]
 
-    parser = argparse.ArgumentParser(description="%s runs SSH logging client example" % progname)
+    parser = argparse.ArgumentParser(
+        description="%s runs SSH logging client example" % progname)
     parser.add_argument('-p', '--port', type=int, default=54973,
                         help="""Port for logging server.""")
     args = parser.parse_args()
